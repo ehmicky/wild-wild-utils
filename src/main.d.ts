bf899d885d261e1d8894b6aaba1713b7624108b2
ehmicky <ehmicky@gmail.com>
2022-03-27 19:55:51 +0200

    Add more types

--- src/main.d.ts
+++ src/main.d.ts
@@ -3,6 +3,8 @@ import type { Target, Query, Entry, Options } from 'wild-wild-path'
 type Value = Entry['value']
 type MapValue = (value: Value) => Value
 type MapEntry = (entry: Entry) => Entry
+type TestValue = (value: Value) => boolean
+type TestEntry = (entry: Entry) => boolean
 
 type OptionsWithEntries = Options & { entries: true }
 
@@ -12,3 +14,9 @@ export function map<T extends Options>(
   mapFunction: T extends OptionsWithEntries ? MapEntry : MapValue,
   options?: T,
 ): Target
+export function find<T extends Options>(
+  target: Target,
+  query: Query,
+  mapFunction: T extends OptionsWithEntries ? TestEntry : TestValue,
+  options?: T,
+): T extends OptionsWithEntries ? Entry : Value
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,12 +1,23 @@
-import { map } from 'wild-wild-utils'
+import { map, find } from 'wild-wild-utils'
 import type { Target, Entry } from 'wild-wild-path'
-import { expectType, expectError } from 'tsd'
+import { expectType, expectNotType, expectError } from 'tsd'
 
 const mapValue = (value: any): any => value
 const mapEntry = (entry: Entry): Entry => entry
+const testValue = (value: any): boolean => true
+const testEntry = (entry: Entry): boolean => true
+
 expectType<Target>(map({}, 'prop', mapValue))
 map({}, 'prop', mapEntry, { entries: true })
 expectError(map(true, 'prop', mapEntry))
 expectError(map({}, true, mapEntry))
 expectError(map({}, 'prop', true))
 expectError(map({}, 'prop', mapEntry, true))
+
+expectType<any>(find({}, 'prop', testValue))
+expectType<Entry>(find({}, 'prop', testEntry, { entries: true }))
+expectNotType<Entry>(find({}, 'prop', testEntry, { entries: false }))
+expectError(find(true, 'prop', testEntry))
+expectError(find({}, true, testEntry))
+expectError(find({}, 'prop', true))
+expectError(find({}, 'prop', testEntry, true))
