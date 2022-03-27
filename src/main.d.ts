99f6b08f009871ba4931cf868fa48a22dd9c48af
ehmicky <ehmicky@gmail.com>
2022-03-27 19:55:35 +0200

    Add more types

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,1 +1,14 @@
-export default function example(): void
+import type { Target, Query, Entry, Options } from 'wild-wild-path'
+
+type Value = Entry['value']
+type MapValue = (value: Value) => Value
+type MapEntry = (entry: Entry) => Entry
+
+type OptionsWithEntries = Options & { entries: true }
+
+export function map<T extends Options>(
+  target: Target,
+  query: Query,
+  mapFunction: T extends OptionsWithEntries ? MapEntry : MapValue,
+  options?: T,
+): Target
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,4 +1,12 @@
-import wildWildUtils from 'wild-wild-utils'
+import { map } from 'wild-wild-utils'
+import type { Target, Entry } from 'wild-wild-path'
 import { expectType, expectError } from 'tsd'
 
-await wildWildUtils()
+const mapValue = (value: any): any => value
+const mapEntry = (entry: Entry): Entry => entry
+expectType<Target>(map({}, 'prop', mapValue))
+map({}, 'prop', mapEntry, { entries: true })
+expectError(map(true, 'prop', mapEntry))
+expectError(map({}, true, mapEntry))
+expectError(map({}, 'prop', true))
+expectError(map({}, 'prop', mapEntry, true))
