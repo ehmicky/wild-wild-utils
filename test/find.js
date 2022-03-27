bc800ea4e0829de62de66830e3e60ec52fece4fb
ehmicky <ehmicky@gmail.com>
2022-03-27 11:56:09 +0200

    Add more tests

--- test/find.js
+++ test/find.js
@@ -6,8 +6,15 @@ const isTwo = function (value) {
   return value === 2
 }
 
+const isDefined = function (value) {
+  return value !== undefined
+}
+
 each(
-  [{ input: [{ one: 1, two: 2 }, '*', isTwo], output: 2 }],
+  [
+    { input: [{ one: 1, two: 2 }, '*', isTwo], output: 2 },
+    { input: [{ one: 1, two: 2 }, '*', isDefined], output: 1 },
+  ],
   ({ title }, { input, output }) => {
     test(`find() output | ${title}`, (t) => {
       t.deepEqual(find(...input), output)
