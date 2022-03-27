7ac82ef56f3188a4ec78a3d019b509b460816924
ehmicky <ehmicky@gmail.com>
2022-03-27 11:56:40 +0200

    Add more tests

--- test/find.js
+++ test/find.js
@@ -6,14 +6,27 @@ const isTwo = function (value) {
   return value === 2
 }
 
-const isDefined = function (value) {
-  return value !== undefined
+const returnTrue = function () {
+  return true
 }
 
 each(
   [
     { input: [{ one: 1, two: 2 }, '*', isTwo], output: 2 },
-    { input: [{ one: 1, two: 2 }, '*', isDefined], output: 1 },
+    { input: [{ one: 1, two: 2 }, '*', returnTrue], output: 1 },
+
+    // `childFirst`, `leaves` and `roots` options
+    { input: [{ one: 1 }, '**', returnTrue], output: { one: 1 } },
+    { input: [{ one: 1 }, '**', returnTrue, { childFirst: true }], output: 1 },
+    { input: [{ one: 1 }, '**', returnTrue, { leaves: true }], output: 1 },
+    {
+      input: [{ one: 1 }, '**', returnTrue, { childFirst: true, roots: true }],
+      output: { one: 1 },
+    },
+
+    // `sort` option
+    { input: [{ two: 2, one: 1 }, '*', returnTrue], output: 2 },
+    { input: [{ two: 2, one: 1 }, '*', returnTrue, { sort: true }], output: 1 },
   ],
   ({ title }, { input, output }) => {
     test(`find() output | ${title}`, (t) => {
