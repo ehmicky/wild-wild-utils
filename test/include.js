28948ffc5fa05353453534e2b0a1a0686d2f93eb
ehmicky <ehmicky@gmail.com>
2022-03-27 13:55:10 +0200

    Add more tests

---
+++ test/include.js
@@ -0,0 +1,88 @@
+import { include } from 'wild-wild-utils'
+
+import { getChild } from './helpers/inherited.js'
+import { testOutput } from './helpers/output.js'
+import { testValidation } from './helpers/validate.js'
+
+const returnFalse = function () {
+  return false
+}
+
+const returnTrue = function () {
+  return true
+}
+
+const isOne = function (value) {
+  return value === 1
+}
+
+const isObject = function (value) {
+  return typeof value === 'object'
+}
+
+const isNotObject = function (value) {
+  return typeof value !== 'object'
+}
+
+const isNamedTwo = function ({ path }) {
+  return path[0] === 'two'
+}
+
+const child = getChild()
+
+testOutput('include', include, [
+  // Main usage
+  {
+    input: [{ one: 1, two: 2 }, 'one two', returnTrue],
+    output: { one: 1, two: 2 },
+  },
+  { input: [{ one: 1, two: 2 }, 'one two', returnFalse], output: {} },
+  { input: [{ one: 1, two: 2 }, 'one', returnTrue], output: { one: 1 } },
+  { input: [{ one: 1, two: 2 }, 'one two', isOne], output: { one: 1 } },
+  { input: [{ one: 1 }, 'two', returnTrue], output: {} },
+  { input: [{ one: { two: 2 } }, 'one one.two', returnFalse], output: {} },
+  {
+    input: [{ one: { two: 2, three: 3 } }, 'one one.two', isObject],
+    output: { one: { two: 2, three: 3 } },
+  },
+  {
+    input: [{ one: { two: 2, three: 3 } }, 'one one.two', isNotObject],
+    output: { one: { two: 2 } },
+  },
+
+  // `entries` option
+  {
+    input: [{ one: 1, two: 2 }, 'one two', isNamedTwo, { entries: true }],
+    output: { two: 2 },
+  },
+
+  // `sort` option
+  {
+    input: [{ two: 2, one: 1 }, 'one two', returnTrue],
+    output: { two: 2, one: 1 },
+  },
+  {
+    input: [{ two: 2, one: 1 }, 'one two', returnTrue, { sort: true }],
+    output: { one: 1, two: 2 },
+  },
+
+  // `classes` and `inherited` options
+  { input: [child, 'own', returnTrue], output: {} },
+  {
+    input: [child, 'own', returnTrue, { classes: true }],
+    output: { own: 'own' },
+  },
+  { input: [child, '/inherited/', returnTrue], output: {} },
+  { input: [child, '/inherited/', returnTrue, { classes: true }], output: {} },
+  {
+    input: [
+      child,
+      '/inherited/',
+      returnTrue,
+      { classes: true, inherited: true },
+    ],
+    output: { inherited: 'inherited' },
+  },
+])
+
+testValidation('include', include, [[{}, true, returnTrue]])
